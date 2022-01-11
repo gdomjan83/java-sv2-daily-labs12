21f3fca48e34badbcd2e60b503097e594c3f8112
J_C <gomjan83@gmail.com>
2022-01-11 21:36:45 +0100

    Day02 excercise updated.

--- src/main/java/day02/MathAlgorithms.java
+++ src/main/java/day02/MathAlgorithms.java
@@ -4,7 +4,7 @@ public class MathAlgorithms {
 
     public int findHighestCommonDivider(int num1, int num2) {
         int result = Math.min(num1, num2);
-        while (!((num2 % result == 0) && (num1 % result == 0))) {
+        while (!(num2 % result == 0 && num1 % result == 0)) {
             result--;
         }
         return result;
